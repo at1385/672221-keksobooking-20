468f4e9192563877b2a142685c7efcae98cc7056
Alexander Tikhonov <at1385@yandex.ru>
2020-07-12 20:36:30 +0500

    Реализует метод отправки данных на сервер

--- js/server.js
+++ js/server.js
@@ -2,6 +2,7 @@
 
 (function () {
   var DOWNLOAD_URL = 'https://javascript.pages.academy/keksobooking/data';
+  var UPLOAD_URL = 'https://javascript.pages.academy/keksobooking';
   var TIMEOUT_IN_MS = 10000;
   var StatusCode = {
     OK: 200
@@ -32,6 +33,31 @@
 
       xhr.open('GET', DOWNLOAD_URL);
       xhr.send();
+    },
+    upload: function (data, onSuccess, onError) {
+      var xhr = new XMLHttpRequest();
+      xhr.responseType = 'json';
+
+      xhr.addEventListener('load', function () {
+        if (xhr.status === StatusCode.OK) {
+          onSuccess(xhr.response);
+        } else {
+          onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+        }
+      });
+
+      xhr.addEventListener('error', function () {
+        onError('Произошла ошибка соединения');
+      });
+
+      xhr.addEventListener('timeout', function () {
+        onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+      });
+
+      xhr.timeout = TIMEOUT_IN_MS;
+
+      xhr.open('POST', UPLOAD_URL);
+      xhr.send(data);
     },
     onError: function (errorMessage) {
       var node = document.createElement('div');
