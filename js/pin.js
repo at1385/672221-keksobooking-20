a1b07c8f8a3099864af2b4db8da973028a5af26f
Alexander Tikhonov <at1385@yandex.ru>
2020-07-17 23:11:45 +0500

    Переносит функцию загрузки объявлений в модуль ads-download.js

--- js/pin.js
+++ js/pin.js
@@ -3,12 +3,10 @@
 (function () {
   var PIN_X_OFFSET = -25;
   var PIN_Y_OFFSET = -70;
-  var ADS_QUANTITY = 5;
 
   var pinsContainer = document.querySelector('.map__pins');
   var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
   var mainPin = pinsContainer.querySelector('.map__pin--main');
-  var pins = [];
 
   var createPin = function (pin) {
     var pinElement = pinTemplate.cloneNode(true);
@@ -24,24 +22,7 @@
   window.pin = {
     container: pinsContainer,
     main: mainPin,
-    render: function (ads) {
-      var fragment = document.createDocumentFragment();
-      var adsLength = ads.length > ADS_QUANTITY ? ADS_QUANTITY : ads.length;
-
-      for (var i = 0; i < adsLength; i++) {
-        fragment.appendChild(createPin(ads[i]));
-      }
-
-      pinsContainer.appendChild(fragment);
-
-      window.util.enableFormElements(window.map.filtersForm, 'select');
-      window.util.enableFormElements(window.map.filtersForm, 'fieldset');
-
-      pins = pinsContainer.querySelectorAll('.map__pin');
-
-      for (i = 1; i < pins.length; i++) {
-        window.card.open(pins[i], ads[i - 1]);
-      }
-    }
+    collection: [],
+    create: createPin
   };
 })();
