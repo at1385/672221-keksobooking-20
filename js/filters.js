8766165e556d15bd85e606c32d0c8de5d79f9034
Alexander Tikhonov <at1385@yandex.ru>
2020-07-19 15:25:15 +0500

    Реализует фильтрацию по типу жилья

--- js/filters.js
+++ js/filters.js
@@ -2,6 +2,28 @@
 
 (function () {
   var filters = document.querySelector('.map__filters');
+  var typeFilter = filters.querySelector('#housing-type');
+
+  var updateAds = function () {
+    var i = 1;
+    window.util.deleteChilds('.map__pins', '.map__pin', i);
+
+    var sameTypeAds = window.ads.array.filter(function (it) {
+      return it.offer.type === typeFilter.value;
+    });
+
+    if (typeFilter.value !== 'any') {
+      window.pin.render(sameTypeAds);
+    } else {
+      window.pin.render(window.ads.array);
+    }
+
+    window.pin.getRelevantCards();
+  };
+
+  typeFilter.addEventListener('change', function () {
+    updateAds();
+  });
 
   window.fiters = {
     form: filters
