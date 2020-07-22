4da9a8d2ba2742a154c575d24a3b2c16802497e3
Alexander Tikhonov <at1385@yandex.ru>
2020-07-22 20:13:46 +0500

    Реализует фильтрацию объявлений всеми фильтрами блока .map__filters

--- js/filters.js
+++ js/filters.js
@@ -3,6 +3,10 @@
 (function () {
   var filters = document.querySelector('.map__filters');
   var typeFilter = filters.querySelector('#housing-type');
+  var priceFilter = filters.querySelector('#housing-price');
+  var roomsFilter = filters.querySelector('#housing-rooms');
+  var guestsFilter = filters.querySelector('#housing-guests');
+  var featuresFilter = filters.querySelector('#housing-features');
 
   var updateAds = function () {
     if (window.card.container.firstChild) {
@@ -12,20 +16,44 @@
     var i = 1;
     window.util.deleteChilds('.map__pins', '.map__pin', i);
 
-    var sameTypeAds = window.ads.array.filter(function (it) {
-      return it.offer.type === typeFilter.value;
-    });
+    var checkedFeatures = Array.from(featuresFilter.querySelectorAll('input:checked'));
 
-    if (typeFilter.value !== 'any') {
-      window.pin.render(sameTypeAds);
-    } else {
-      window.pin.render(window.ads.array);
-    }
+    window.pin.render(window.ads.array
+      .filter(function (ad) {
+        var typeAds = (typeFilter.value !== 'any') ? ad.offer.type === typeFilter.value : window.ads.array;
+        return typeAds;
+      })
+      .filter(function (ad) {
+        switch (priceFilter.value) {
+          case 'middle':
+            return ad.offer.price >= 10000 && ad.offer.price <= 50000;
+          case 'low':
+            return ad.offer.price < 10000;
+          case 'high':
+            return ad.offer.price > 50000;
+          default:
+            return ad.offer.price;
+        }
+      })
+      .filter(function (ad) {
+        var roomsAds = (roomsFilter.value !== 'any') ? ad.offer.rooms === +roomsFilter.value : window.ads.array;
+        return roomsAds;
+      })
+      .filter(function (ad) {
+        var guestsAds = (guestsFilter.value !== 'any') ? ad.offer.guests === +guestsFilter.value : window.ads.array;
+        return guestsAds;
+      })
+      .filter(function (ad) {
+        return checkedFeatures.every(function (feature) {
+          return ad.offer.features.includes(feature.value);
+        });
+      })
+    );
 
     window.pin.getRelevantCards();
   };
 
-  typeFilter.addEventListener('change', function () {
+  filters.addEventListener('change', function () {
     updateAds();
   });
 
