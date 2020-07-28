751f3cb41f930fe5a980f7029af31bb5328b16b1
Alexander Tikhonov <at1385@yandex.ru>
2020-07-28 19:21:50 +0500

    Переносит метод сброса страницы в исходное состояниие в отдельный модуль

--- js/ad-form-send.js
+++ js/ad-form-send.js
@@ -28,30 +28,7 @@
 
   window.adFormSend = {
     onSuccess: function () {
-      window.util.setDefaultCoords();
-
-      if (window.card.container.firstChild) {
-        window.card.close();
-      }
-
-      for (var i = 1; i < window.pin.collection.length; i++) {
-        window.pin.container.removeChild(window.pin.collection[i]);
-      }
-
-      window.map.block.classList.add('map--faded');
-      window.util.disableFormElements(window.fiters.form, 'select');
-      window.util.disableFormElements(window.fiters.form, 'fieldset');
-      window.fiters.form.reset();
-
-      window.adForm.form.classList.add('ad-form--disabled');
-      window.util.disableFormElements(window.adForm.form, 'fieldset');
-      window.adForm.form.reset();
-      window.adFormImages.reset();
-      window.getAdressCoords(window.pin.main.offsetLeft, window.pin.main.offsetTop, window.adForm.form, window.adForm.address);
-
-      window.pin.main.addEventListener('mousedown', window.map.onMainPinLeftClick);
-      window.pin.main.addEventListener('keydown', window.map.onMainPinEnterPress);
-
+      window.setOriginalState();
       showMessage('#success', '.success');
     },
     onError: function () {
