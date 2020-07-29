657451df7d5605515f503eb29ba54883f1234387
Alexander Tikhonov <at1385@yandex.ru>
2020-07-30 00:38:34 +0500

    Переписывает фильтры и реализует своевременный выход из цикла

--- js/filters.js
+++ js/filters.js
@@ -4,9 +4,18 @@
   var FILTER_DEFAULT_VALUE = 'any';
 
   var PriceRange = {
-    LOW: 'low',
-    MIDDLE: 'middle',
-    HIGH: 'high'
+    LOW: {
+      MIN: 0,
+      MAX: 10000
+    },
+    MIDDLE: {
+      MIN: 10000,
+      MAX: 50000
+    },
+    HIGH: {
+      MIN: 50000,
+      MAX: Infinity
+    }
   };
 
   var filters = document.querySelector('.map__filters');
@@ -19,6 +28,57 @@
   window.util.disableFormElements(filters, 'select');
   window.util.disableFormElements(filters, 'fieldset');
 
+  var filterByType = function (ad) {
+    if (typeFilter.value === FILTER_DEFAULT_VALUE || typeFilter.value === ad.offer.type) {
+      return true;
+    }
+    return false;
+  };
+
+  var filterByPrice = function (ad) {
+    if (priceFilter.value === FILTER_DEFAULT_VALUE || (PriceRange[priceFilter.value.toUpperCase()].MIN <= ad.offer.price && PriceRange[priceFilter.value.toUpperCase()].MAX >= ad.offer.price)) {
+      return true;
+    }
+    return false;
+  };
+
+  var filterByRooms = function (ad) {
+    if (roomsFilter.value === FILTER_DEFAULT_VALUE || +roomsFilter.value === ad.offer.rooms) {
+      return true;
+    }
+    return false;
+  };
+
+  var filterByGuests = function (ad) {
+    if (guestsFilter.value === FILTER_DEFAULT_VALUE || +guestsFilter.value === ad.offer.guests) {
+      return true;
+    }
+    return false;
+  };
+
+  var filterByFeatures = function (ad) {
+    var checkedFeatures = Array.from(featuresFilter.querySelectorAll('input:checked'));
+
+    return checkedFeatures.every(function (feature) {
+      return ad.offer.features.includes(feature.value);
+    });
+  };
+
+  var getFilteredAds = function (ads) {
+    var filteredAds = [];
+
+    for (var i = 0; i < ads.length; i++) {
+      if (filterByType(ads[i]) && filterByPrice(ads[i]) && filterByRooms(ads[i]) && filterByGuests(ads[i]) && filterByFeatures(ads[i])) {
+        filteredAds.push(ads[i]);
+        if (filteredAds.length === window.pin.QUANTITY) {
+          break;
+        }
+      }
+    }
+
+    return filteredAds;
+  };
+
   var updateAds = function () {
     if (window.card.container.firstChild) {
       window.card.close();
@@ -27,37 +87,7 @@
     var i = 1;
     window.util.deleteChilds('.map__pins', '.map__pin', i);
 
-    var checkedFeatures = Array.from(featuresFilter.querySelectorAll('input:checked'));
-
-    var filterByPrice = function (ad) {
-      switch (priceFilter.value) {
-        case PriceRange.MIDDLE:
-          return ad.offer.price >= 10000 && ad.offer.price <= 50000;
-        case PriceRange.LOW:
-          return ad.offer.price < 10000;
-        case PriceRange.HIGH:
-          return ad.offer.price > 50000;
-        default:
-          return ad.offer.price;
-      }
-    };
-
-    var filterByFeatures = function (ad) {
-      return checkedFeatures.every(function (feature) {
-        return ad.offer.features.includes(feature.value);
-      });
-    };
-
-    window.pin.render(window.ads.data
-      .filter(function (ad) {
-        return ((typeFilter.value !== FILTER_DEFAULT_VALUE) ? ad.offer.type === typeFilter.value : window.ads.data) &&
-        filterByPrice(ad) &&
-        ((roomsFilter.value !== FILTER_DEFAULT_VALUE) ? ad.offer.rooms === +roomsFilter.value : window.ads.data) &&
-        ((guestsFilter.value !== FILTER_DEFAULT_VALUE) ? ad.offer.guests === +guestsFilter.value : window.ads.data) &&
-        filterByFeatures(ad);
-      })
-    );
-
+    window.pin.render(getFilteredAds(window.ads.data));
     window.pin.getRelevantCards();
   };
 
